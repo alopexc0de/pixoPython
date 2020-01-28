86a8d7cd563979f74f1e0fad1354b3945e4e4a78
David Todd <c0de@c0defox.es>
2020-01-27 19:01:14 -0600

    switch to Hardware SPI

--- pixo/board.py
+++ pixo/board.py
@@ -8,5 +8,5 @@ DOTSTAR_TX = Pin(19)
 DOTSTAR_RX = Pin(17) # Note: This is unused as the LEDs don't send data back
 
 # Setup the LED board
-DOTSTAR_SPI = SPI(sck=DOTSTAR_CLOCK, mosi=DOTSTAR_TX, miso=DOTSTAR_RX)
+DOTSTAR_SPI = SPI(2, sck=DOTSTAR_CLOCK, mosi=DOTSTAR_TX, miso=DOTSTAR_RX)
 BOARD = dotstar.DotStar(DOTSTAR_SPI, TOTAL_DOTS, brightness=0.2, auto_write=False)
